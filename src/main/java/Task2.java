ef634d59dabaa1863e1666685e496620dac55481
“LisinMihail” <mslfoxgit@gmail.com>
2022-05-30 23:12:47 +0600

    All Tasks are solved

---
+++ src/main/java/Task2.java
@@ -0,0 +1,44 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.script.SimpleBindings;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task2 {
+    protected static void startTask() throws ParserConfigurationException, IOException, SAXException {
+        List<Employee> employeeList = parseXML("data.xml");
+        Task1.writeString(Task1.listToJson(employeeList ), "data2.json");
+    }
+
+    protected static List<Employee> parseXML(String file) throws ParserConfigurationException, IOException, SAXException {
+        List<Employee> employeeList = new ArrayList<>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(file));
+        NodeList nodeList = doc.getElementsByTagName("employee");
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            String employeeString = nodeList.item(i).getTextContent().strip();
+            String[] employeeStringArr = employeeString.replaceAll(" ", "").split("\n");
+            Employee employee = new Employee(
+                    Long.parseLong(employeeStringArr[0]),
+                    employeeStringArr[1],
+                    employeeStringArr[2],
+                    employeeStringArr[3],
+                    Integer.parseInt(employeeStringArr[4]));
+            employeeList.add(employee);
+        }
+
+        return employeeList;
+    }
+}
+
---
+++ src/main/java/Task3.java
@@ -0,0 +1,39 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Task3 {
+    protected static void startTask() throws FileNotFoundException {
+        String json = readString("data.json");
+        List<Employee> list = jsonToList(json);
+        list.forEach(System.out::println);
+    }
+
+    private static List<Employee> jsonToList(String json) throws FileNotFoundException {
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        List<Employee> list = new Gson().fromJson(json, listType);
+        return list;
+    }
+
+    private static String readString(String filename) {
+        StringBuilder jsonBiulder = new StringBuilder();
+        try (BufferedReader input = new BufferedReader(new FileReader(filename))) {
+            List<String> str = input.lines().collect(Collectors.toList());
+            for (String item : str) {
+                jsonBiulder.append(item);
+            }
+        } catch (IOException e) {
+            e.getMessage();
+        }
+        return jsonBiulder.toString();
+    }
+}
